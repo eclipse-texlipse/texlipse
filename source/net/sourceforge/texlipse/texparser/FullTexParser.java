5c1d5a6eebc4f1ddc954f8411994545f49c0f99f
Boris von Loesch <boris@vonloesch.de>
2006-03-23 20:13:24 +0000

    Fixed some bugs in FullOutline (inclusion of files with file endings, repeatedly parsing of the unchanged document, problems with last element in a file)

--- source/net/sourceforge/texlipse/texparser/FullTexParser.java
+++ source/net/sourceforge/texlipse/texparser/FullTexParser.java
@@ -26,7 +26,7 @@ import org.eclipse.core.runtime.CoreException;
  * changes it will only parse the changed document and its inputs.
  */
 public class FullTexParser extends TexParser {
-
+    
     private IProject currentProject;
     private ArrayList outlineTree;
     private OutlineNode parent;
@@ -34,9 +34,9 @@ public class FullTexParser extends TexParser {
     private IFile mainFile;
     private IFile changedFile;
     private String changedInput;
-
+    
     private static String TEX_FILE_ENDING = ".tex";
-
+    
     /**
      * 
      * @param currentProject the current project.
@@ -50,7 +50,7 @@ public class FullTexParser extends TexParser {
         this.currentProject = currentProject;
         this.position = -1;
     }
-
+    
     /**
      * Parses the document. Parses the complete project with its inputs recursively.
      * At the first round the complete project is parsed. Then only the changed
@@ -60,7 +60,7 @@ public class FullTexParser extends TexParser {
      * @param bibs the bib container.
      */
     public void parseDocument(ReferenceContainer labels, ReferenceContainer bibs)
-            throws IOException {
+    throws IOException {
         try {
             // parse partially
             if (changedFile != null && changedInput != null
@@ -74,12 +74,12 @@ public class FullTexParser extends TexParser {
                             new StringReader(changedInput), 1024));
                     LatexParser inputParser = new LatexParser();
                     inputParser.parse(lexer, labels, bibs);
-
+                    
                     // add the nodes to the tree
                     ArrayList newNodes = inputParser.getOutlineTree();
                     addFileNames(newNodes, changedFile);
                     addToTree(parent.getChildren(), newNodes, null, null);
-
+                    
                     // parse inputs recursively
                     if (inputParser.getInputs().size() > 0) {
                         processInputs(newNodes, inputParser.getInputs(),
@@ -105,7 +105,7 @@ public class FullTexParser extends TexParser {
                     }
                 }
                 this.extractPreamble(input);
-
+                
                 // start the parse
                 LatexLexer lexer = new LatexLexer(new PushbackReader(
                         new StringReader(input), 1024));
@@ -116,10 +116,10 @@ public class FullTexParser extends TexParser {
                 } else {
                     lparser.parse(lexer, labels, bibs);
                 }
-
+                
                 this.outlineTree = lparser.getOutlineTree();
                 this.fatalErrors = lparser.isFatalErrors();
-
+                
                 // add the file references and parse the inputs recursively
                 addFileNames(this.outlineTree, this.mainFile);
                 if (lparser.getInputs().size() > 0) {
@@ -142,7 +142,7 @@ public class FullTexParser extends TexParser {
             this.fatalErrors = true;
         }
     }
-
+    
     /**
      * Cleans up the the variables needed for the partial parse.
      */
@@ -152,7 +152,7 @@ public class FullTexParser extends TexParser {
         this.changedFile = null;
         this.changedInput = null;
     }
-
+    
     /**
      * Deletes the nodes of the document which changed from the outline tree.
      * @param parent the OutlineNode whose children are to be deleted.
@@ -167,7 +167,7 @@ public class FullTexParser extends TexParser {
             }
         }
     }
-
+    
     /**
      * Searches the outlineTree for the OutlineNode and the position where the
      * changed input is added recursively.
@@ -189,7 +189,7 @@ public class FullTexParser extends TexParser {
                 return;
         }
     }
-
+    
     /**
      * Reads a file from the project.
      * @param file the file to be read.
@@ -209,7 +209,7 @@ public class FullTexParser extends TexParser {
         buf.close();
         return this.rmTrailingWhitespace(inputContent);
     }
-
+    
     /**
      * Processes the inputs and adds them to the outlineTree recursively.
      * @param tree The children of a node, where the input nodes should be added.
@@ -223,13 +223,15 @@ public class FullTexParser extends TexParser {
      */
     private ArrayList processInputs(ArrayList tree, ArrayList inputs,
             IFile inputFile, ReferenceContainer labels, ReferenceContainer bibs)
-            throws IOException, LexerException {
+    throws IOException, LexerException {
         OutlineNode inputNode;
         for (int i = 0; i < inputs.size(); i++) {
             inputNode = (OutlineNode) inputs.get(i);
-            IFile file = currentProject.getFile(inputNode.getName()
-                    + TEX_FILE_ENDING);
-
+            String fileName = inputNode.getName();
+            if (fileName.indexOf('.') == -1) 
+                fileName += TEX_FILE_ENDING;
+            IFile file = currentProject.getFile(fileName);
+            
             String inputContent;
             try {
                 // read the input
@@ -239,8 +241,8 @@ public class FullTexParser extends TexParser {
                     // only add an error if the input is in the current editor
                     // input, because of the marker.
                     String editorInput = TexlipsePlugin
-                            .getCurrentWorkbenchPage().getActiveEditor()
-                            .getEditorInput().getName();
+                    .getCurrentWorkbenchPage().getActiveEditor()
+                    .getEditorInput().getName();
                     if (editorInput.equals(inputNode.getParent().getIFile()
                             .getName())) {
                         this.errors = new ArrayList(1);
@@ -260,13 +262,13 @@ public class FullTexParser extends TexParser {
                         new StringReader(inputContent), 1024));
                 LatexParser inputParser = new LatexParser();
                 inputParser.parse(lexer, labels, bibs);
-
+                
                 // add the new nodes to the tree
                 ArrayList newNodes = inputParser.getOutlineTree();
                 if (newNodes.size() > 0) {
                     addFileNames(newNodes, file);
                     addToTree(tree, newNodes, inputNode, inputFile);
-
+                    
                     // parse inputs recursively
                     if (inputParser.getInputs().size() > 0) {
                         processInputs(newNodes, inputParser.getInputs(), file,
@@ -277,7 +279,7 @@ public class FullTexParser extends TexParser {
         }
         return tree;
     }
-
+    
     /**
      * Adds the nodes to the tree.
      * @param tree the part of the tree, where the nodes should be added.
@@ -310,7 +312,7 @@ public class FullTexParser extends TexParser {
             }
         }
     }
-
+    
     /**
      * Gets the position where the nodes should be added. Either searches for the
      * position or returns it when the position is already available(in case of
@@ -325,18 +327,18 @@ public class FullTexParser extends TexParser {
         // (only at the changed file)
         if (this.position != -1)
             return position;
-
+        
         // if empty add them to the front
         if (tree == null || tree.size() == 0)
             return 0;
-
+        
         // input is at the front of the document
         OutlineNode beginNode = (OutlineNode) tree.get(0);
         if (beginNode.getIFile().equals(file)
                 && inputNode.getBeginLine() < beginNode.getBeginLine()) {
             return 0;
         }
-
+        
         // search the list for the correct position.
         // take care only of the nodes, which are from the file the input belongs to.
         OutlineNode pre = null;
@@ -358,7 +360,7 @@ public class FullTexParser extends TexParser {
         // when no position is found, the input must be at the end.
         return tree.size();
     }
-
+    
     /**
      * Adds the file references to the tree.
      * @param tree the tree to add the file references.
@@ -373,7 +375,7 @@ public class FullTexParser extends TexParser {
             addFileNames(on.getChildren(), file);
         }
     }
-
+    
     /**
      * Prints out the tree to the given depth recursively. 
      * Only for debugging purposes.
@@ -398,14 +400,14 @@ public class FullTexParser extends TexParser {
             showTree(outnode.getChildren(), depth--, identation.concat("    "));
         }
     }
-
+    
     /**
      * @return the outlineTree.
      */
     public ArrayList getOutlineTree() {
         return this.outlineTree;
     }
-
+    
     /**
      * 
      * @param changedFile the file reference to the file which changed.
@@ -413,7 +415,7 @@ public class FullTexParser extends TexParser {
     public void setChangedFile(IFile changedFile) {
         this.changedFile = changedFile;
     }
-
+    
     /**
      * 
      * @param changedInput the changed input.

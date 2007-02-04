a9e75922055e412dae844a718ff39563441145b5
Boris von Loesch <boris@vonloesch.de>
2007-02-04 18:39:32 +0000

    New hard line wrap

--- source/net/sourceforge/texlipse/editor/HardLineWrap.java
+++ source/net/sourceforge/texlipse/editor/HardLineWrap.java
@@ -9,8 +9,6 @@
  */
 package net.sourceforge.texlipse.editor;
 
-import net.sourceforge.texlipse.TexlipsePlugin;
-
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
@@ -22,118 +20,17 @@ import org.eclipse.jface.text.IRegion;
  * 
  * @author Antti Pirinen
  * @author Oskar Ojala
+ * @author Boris von Loesch
  */
 public class HardLineWrap {
-    //private int lineLength = 80;
     
-    private boolean lastCharOfLine; // True if character is the last of the current line
-    private boolean lastCharOfDoc;  // True if character is the last character of document
-    private int carPos;	 // The position of the caret
-    private int wsLast;  // The last white space position before MAX
-    private int wsFirst; // The first white space position after MAX
-    private int indentLength;    // The length of the indentation of the current line
-    private int nextLineLength;  // The length of next line 
-    private int currLineLength;  // The length of current line
-    private int MAX;             // The maximum length of line
-    private String currLineTxt;  // The text of current line before modification
-    private String ld = "\n";    // The default line delimiter
     private TexEditorTools tools;
     
     public HardLineWrap(){
         this.tools = new TexEditorTools();
     }
-    /**
-     * The actual wrapping method. Based on the <code>IDocument d</code>
-     * and <code>DocumentCommand c</code> the method determines how the
-     * line must be wrapped. 
-     * <p>
-     * If there is more than <code>MAX_LENGTH</code>
-     * characters at the line, the method tries to detect the last white
-     * space before <code> MAX_LENGTH</code>. In case there is none, the 
-     * method finds the first white space after <code> MAX_LENGTH</code>.
-     * If there is enoght room at the next line, the end of current line 
-     * will be added to the next line. Otherwise the new line is added.
-     * 
-     * @param d 			IDocument
-     * @param c 			DocumentCommand
-     * @param MAX_LENGTH 	How many characters are allowed at one line.
-     */
-    public void doWrap(IDocument d, DocumentCommand c, int MAX_LENGTH) {
-        //int counter = 0;
-        MAX = MAX_LENGTH; 
-        //counter = tools.getLineLength(d,c,false);
-        currLineLength = tools.getLineLength(d,c,false,0);
-        nextLineLength = tools.getLineLength(d,c,false,1);
-        indentLength   = tools.getIndentation(d,c).length();
-        lastCharOfLine = (tools.getIndexAtLine(d,c, false) == -1 ? true: false);			
-        
-        if (currLineLength >= MAX && (tools.isWhiteSpace(c) || (
-                tools.isLineDelimiter(d,c) && lastCharOfLine))) {
-            
-            ld = tools.getLineDelimiter(d,c);
-            currLineTxt = tools.getStringAt(d,c,true);
-            carPos = tools.getIndexAtLine(d,c,true);
-            wsLast = tools.getLastWSPosition(currLineTxt, MAX);
-            wsFirst = tools.getFirstWSPosition(currLineTxt, MAX);
-            lastCharOfDoc  = (tools.getIndexAtLine(d,c, true) == -1 ? true: false);
-            if( lastCharOfDoc) {
-                carPos = d.getLength();
-                try {
-                    d.replace(d.getLength(), 0, "\n");
-                } catch(BadLocationException e) {
-                    System.out.println(e); // FIXME
-                }
-            }
-            
-            if ((wsLast < carPos && carPos <= MAX && carPos > indentLength) 
-                    ||
-                    (wsLast < indentLength && carPos <= wsFirst && carPos > indentLength)
-                    ||		
-                    (wsLast < indentLength && carPos >= MAX)){
-                /*                 MAX  
-                 * >###a#CaaaaaaaaaC|aaaa#aaaa
-                 *       ^~~~~~~~~~^|  
-                 *
-                 *  OR
-                 *               MAX  
-                 * >###aCaaaaaaaaa|aaaC#aaaa
-                 *      ^~~~~~~~~~|~~~^
-                 * 
-                 *  OR
-                 *              MAX  
-                 * >aaaaaaaaaaaaa|aaaCa#aaaa
-                 *               |   ^
-                 *    
-                 */
-                
-                wrapCase1(d,c,carPos);
-            } else if (carPos <= wsLast){
-                /*                 MAX  
-                 * >###CaaaaaaaaC#a|aaaa#aaaa
-                 *     ^~~~~~~~~^  |  
-                 */
-                
-                wrapCase2(d,c,carPos, wsLast);
-            } else if(wsLast > indentLength && carPos > MAX ){
-                /*               MAX  
-                 * >###aaaaa#aaaaa|Caaa#aaaCaa
-                 *                 ^~~~~~~~^
-                 */
-                
-                wrapCase3(d, c, wsLast, carPos);
-            } else {
-                /*               MAX  
-                 * >###aaaaaaaaaaa|a#Caa#aCaaaa
-                 *                   ^~~~~^
-                 */
-                
-                wrapCase3(d,c,wsFirst,carPos);
-            }
-        }	
-    }
-    
 
-    public void doWrapB(IDocument d, DocumentCommand c, int MAX_LENGTH) {
+/*    public void doWrapB(IDocument d, DocumentCommand c, int MAX_LENGTH) {
         try {
             // Get the line of the command excluding delimiter
             IRegion commandRegion = d.getLineInformationOfOffset(c.offset);
@@ -155,6 +52,7 @@ public class HardLineWrap {
 
             // Check if the cursor is not at the end of the line
             boolean caretMidLine = false;
+            int carretCorrection = 0;
             int cursorOnLine = c.offset - commandRegion.getOffset();
             if (cursorOnLine != commandRegion.getLength()) {
                 caretMidLine = true;
@@ -165,10 +63,15 @@ public class HardLineWrap {
                 } else {
                     // We modify the line and everything is handled automatically
                     String startLine = line.substring(0, cursorOnLine);
+                    carretCorrection += line.length() - cursorOnLine - line.substring(cursorOnLine).trim().length();
                     line = startLine + c.text + line.substring(cursorOnLine);
                 }
             }
-
+            else{
+                line = line + c.text;
+            }
+                
+            int trimCorrection = 0;
             // Break the line
             if (breakpos < indent.length()) {
                 buf.append(delim);
@@ -181,17 +84,21 @@ public class HardLineWrap {
             } else {
                 buf.append(delim);
                 buf.append(indent);
-                buf.append(line.substring(breakpos + 1).trim()); // TODO trim?
+                String newText = line.substring(breakpos + 1).trim(); // TODO trim?
+                buf.append(newText); 
+                trimCorrection -= line.length() - breakpos - 1 - newText.trim().length();
                 //buf.append(" ");
             }
             
             // If the cursor was at the end of the line, add the typed text
-            if (!caretMidLine) {
-                buf.append(c.text);
+            carretCorrection += trimCorrection;
+            if (!caretMidLine && (c.text.endsWith(" ") || c.text.endsWith("\t"))) {
+                buf.append(c.text.charAt(c.text.length()-1));
+                carretCorrection++;
             }
-
-            int indentLengthCorrection = 0;
             
+            int indentLengthCorrection = 0;
+
             // Append next line or insert newline
             if (nextline.length() > 0
                     && !tools.isLineCommandLine(nextline)
@@ -200,6 +107,7 @@ public class HardLineWrap {
                     && indent.indexOf('%') == -1) {
                 if (caretMidLine) {
                     buf.append(" ");
+                    carretCorrection++;
                 }
                 buf.append(nextline);
             } else {
@@ -215,7 +123,7 @@ public class HardLineWrap {
 
             //c.offset = d.getLineOffset(d.getLineOfOffset(c.offset)) + breakpos;
             if (cursorOnLine > breakpos) {
-                c.caretOffset = c.offset + delim.length() + indent.length();
+                c.caretOffset = c.offset + delim.length() + indent.length() + carretCorrection;
                 if (breakpos > indent.length()) {
                     c.offset = commandRegion.getOffset() + breakpos;
                 } else {
@@ -227,8 +135,122 @@ public class HardLineWrap {
             }
             
             //c.length = buf.length() - 1 - indent.length();
-            c.length = buf.length() - delim.length() - indentLengthCorrection;
+            c.length = buf.length() - delim.length() - indentLengthCorrection - trimCorrection;
             //c.caretOffset = newCaretOffset;
+            c.text = buf.toString();
+            
+        } catch(BadLocationException e) {
+            // FIXME
+       }
+    }*/
+    
+    /**
+     * Removes all whitespaces from the beginning of the String
+     * @param str The string to wrap
+     * @return trimmed version of the string
+     */
+    private static String trimBegin (final String str) {
+        int i = 0;
+        //while (i < str.length() && (str.charAt(i) == ' ' || str.charAt(i) == '\t')) 
+       while (i < str.length() && (Character.isWhitespace(str.charAt(i)))) 
+            i++;
+        return str.substring(i);
+    }
+    
+    /**
+     * Removes all whitespaces from the end of the String
+     * @param str The string to wrap
+     * @return trimmed version of the string
+     */
+    private static String trimEnd (final String str) {
+        int i = str.length() - 1;
+        //while (i >= 0 && (str.charAt(i) == ' ' || str.charAt(i) == '\t')) 
+        while (i >= 0 && (Character.isWhitespace(str.charAt(i)))) 
+            i--;
+        return str.substring(0, i + 1);
+    }
+
+    /**
+     * New line wrapping strategy.    
+     * The actual wrapping method. Based on the <code>IDocument d</code>
+     * and <code>DocumentCommand c</code> the method determines how the
+     * line must be wrapped. 
+     * <p>
+     * If there is more than <code>MAX_LENGTH</code>
+     * characters at the line, the method tries to detect the last white
+     * space before <code> MAX_LENGTH</code>. In case there is none, the 
+     * method finds the first white space after <code> MAX_LENGTH</code>.
+     * Normally it adds the rest of the currentline to the next line. 
+     * Exceptions are empty lines, commandlines, commentlines, and special lines like \\ or \[.
+     * TODO Make the length of c as small as possible, because otherwise this can delete markers.
+     * 
+     * @param d             IDocument
+     * @param c             DocumentCommand
+     * @param MAX_LENGTH    How many characters are allowed at one line.
+     */
+    public void doWrapB(IDocument d, DocumentCommand c, int MAX_LENGTH) {
+        try {
+            // Get the line of the command excluding delimiter
+            IRegion commandRegion = d.getLineInformationOfOffset(c.offset);
+            
+            // Ignore texts with line endings
+            if (commandRegion.getLength() + c.text.length() <= MAX_LENGTH || 
+                    c.text.indexOf("\n") >= 0 || c.text.indexOf("\r") >= 0) return;
+            
+            String line = d.get(commandRegion.getOffset(), commandRegion.getLength());
+            int lineNr = d.getLineOfOffset(c.offset);
+            String delim = d.getLineDelimiter(lineNr);
+            //String indent = tools.getIndentation(d, c); // TODO check if inside comment
+            String indent = tools.getIndentationWithComment(line);
+            String nextline = tools.getStringAt(d, c, false, 1);
+            final int cursorOnLine = c.offset - commandRegion.getOffset();
+            
+            //Create the newLine, we rewrite the whole currentline
+            StringBuffer newLineBuf = new StringBuffer();
+            int length = line.length();
+            
+            newLineBuf.append(line.substring(0, cursorOnLine));
+            newLineBuf.append (c.text);
+            newLineBuf.append(trimEnd(line.substring(cursorOnLine)));
+            String nextTrimLine = nextline.trim(); 
+            
+            if (nextTrimLine.length() > 0
+                    && !tools.isLineCommandLine(nextTrimLine)
+                    && !tools.isLineCommentLine(nextTrimLine)
+                    && !tools.isLineItemLine(nextTrimLine)
+                    && !((nextTrimLine.startsWith("\\") && nextTrimLine.length() == 2)) // e.g. \\ or \[
+                    && indent.indexOf('%') == -1) {
+                //Add the whole next line
+                newLineBuf.append(' ');
+                newLineBuf.append(trimBegin(nextline));
+                length += nextline.length();
+            } else {
+                newLineBuf.append(delim);
+            }
+            length += delim.length(); //delim.length();
+            String newLine = newLineBuf.toString();
+
+            c.length = length;
+            
+            int breakpos = tools.getLastWSPosition(newLine, MAX_LENGTH);
+            c.shiftsCaret = false;
+            c.caretOffset = c.offset + c.text.length() + indent.length();
+            if (breakpos > cursorOnLine){ 
+                c.caretOffset -= indent.length();
+            }
+            if (breakpos < cursorOnLine){
+                //Line delimiter - one white space
+                c.caretOffset += delim.length() - 1;
+            }
+
+            c.offset = commandRegion.getOffset();
+
+            StringBuffer buf = new StringBuffer();
+            buf.append(newLine.substring(0, breakpos));
+            buf.append(delim);
+            buf.append(indent);
+            buf.append(trimBegin(newLine.substring(breakpos)));
+            
             c.text = buf.toString();
             
         } catch(BadLocationException e) {
@@ -236,154 +258,4 @@ public class HardLineWrap {
        }
     }
     
-    /**                MAX  
-     * >###a#CaaaaaaaaaC|aaaa#aaaa
-     *       ^~~~~~~~~~^|  
-     *
-     *  OR
-     *               MAX  
-     * >###aCaaaaaaaaa|aaaC#aaaa
-     *      ^~~~~~~~~~|~~~^  
-     *
-     * @param d
-     * @param c
-     */
-    private void wrapCase1(IDocument d, DocumentCommand c, int txtStart) {
-        int newCaretOffset;
-        c.shiftsCaret = false;
-        StringBuffer buf = new StringBuffer();
-        
-        String endLine = currLineTxt.substring(txtStart);
-        if (carPos >= (MAX-1)){
-            newCaretOffset = getNewCaretOffset(d, c);
-        } else {
-            newCaretOffset = c.offset +1;
-            buf.append(" ");
-        }
-        buf.append(ld);
-        buf.append(tools.getIndentation(d,c));
-        String nextLineTxt = tools.getStringAt(d,c,false,1);
-        
-        if ( nextLineTxt.trim().length() == 0 ||
-                (nextLineLength +1+ endLine.trim().length()) > MAX  ||
-                tools.isLineCommandLine(d,c,1) || 
-                tools.isLineCommentLine(d,c,1) ||
-                tools.isLineItemLine(d,c,1)) {
-            // Create new line
-            buf.append(endLine);
-            c.length = endLine.length();
-            
-        } else {
-            // Add the text to the next line
-            nextLineTxt = tools.getStringAt(d,c,false,1);
-            nextLineTxt = tools.trimBegin(nextLineTxt);
-            buf.append(endLine.trim());
-            buf.append(" ");
-            buf.append(nextLineTxt);
-            c.length = endLine.length()+nextLineLength;
-        }
-        c.caretOffset = newCaretOffset;
-        c.text = buf.toString();
-    }
-    
-    /**                 MAX  
-     * >###CaaaaaaaaC#a|aaaa#aaaa
-     *     ^~~~~~~~~^  |  
-     */
-    private void wrapCase2(IDocument d, DocumentCommand c, int txtStart, int lineEnd){
-        int newCaretOffset;
-        c.shiftsCaret = false;
-        StringBuffer buf = new StringBuffer();
-        String midLine = currLineTxt.substring(txtStart,lineEnd);
-        String endLine = currLineTxt.substring(lineEnd+1);
-        newCaretOffset = c.offset +1;
-        buf.append(" ");
-        buf.append(midLine);
-        buf.append(ld);
-        buf.append(tools.getIndentation(d,c));
-        
-        String nextLineTxt = tools.getStringAt(d,c,false,1);
-        if ( nextLineTxt.trim().length() == 0 ||  
-                (nextLineLength +1+ endLine.trim().length()) > MAX  ||
-                tools.isLineCommandLine(d,c,1) || 
-                tools.isLineCommentLine(d,c,1) ||
-                tools.isLineItemLine(d,c,1)) {
-            // Create new line
-            buf.append(endLine);
-            c.length = tools.getLineLength(d,c,true)-carPos;
-            
-        } else {
-            // Add the text to the next line
-            nextLineTxt = tools.getStringAt(d,c,false,1);
-            nextLineTxt = tools.trimBegin(nextLineTxt);
-            buf.append(endLine.trim());
-            buf.append(" ");
-            buf.append(nextLineTxt);
-            c.length = tools.getLineLength(d,c,true)-carPos +nextLineLength;
-        }
-        c.caretOffset = newCaretOffset;
-        c.text = buf.toString();
-    }
-    
-    /**              MAX  
-     * >###aaaaa#aaaaa|Caaa#aaaCaa
-     *                 ^~~~~~~~^
-     */
-    private void wrapCase3(IDocument d, DocumentCommand c, int txtStart, int lineEnd){
-        int newCaretOffset;
-        c.shiftsCaret = false;
-        StringBuffer buf = new StringBuffer();
-        String line    = currLineTxt.substring(txtStart);
-        String midLine = currLineTxt.substring(txtStart+1,lineEnd);
-        String endLine = currLineTxt.substring(lineEnd);
-        newCaretOffset = getNewCaretOffset(d,c);
-        
-        buf.append(ld);
-        buf.append(tools.getIndentation(d,c));
-        
-        String nextLineTxt = tools.getStringAt(d,c,false,1);
-        if ( nextLineTxt.trim().length() == 0 ||  
-                (nextLineLength +1+ line.trim().length()) > MAX      ||
-                tools.isLineCommandLine(d,c,1) || 
-                tools.isLineCommentLine(d,c,1) ||
-                tools.isLineItemLine(d,c,1)){
-            // Create new line
-            buf.append(midLine);
-            buf.append(" ");
-            buf.append(endLine);
-            c.length = line.length();
-            //System.out.println("No new line");
-            
-        } else {
-            // Add the text to the next line
-            nextLineTxt = tools.getStringAt(d,c,false,1);
-            nextLineTxt = tools.trimBegin(nextLineTxt);
-            buf.append(midLine);
-            buf.append(" ");
-            buf.append(endLine.trim());
-            buf.append(" ");
-            buf.append(nextLineTxt);
-            c.length = line.length() +nextLineLength;			
-        }
-        
-        try{
-            c.offset = d.getLineOffset(d.getLineOfOffset(c.offset))+txtStart;
-        }catch(BadLocationException e){
-            TexlipsePlugin.log("HardLineWrap.wrapCase3", e);
-        }
-        c.caretOffset = newCaretOffset;
-        c.text = buf.toString();
-    }
-    
-    /**
-     * The method return the offset of the caret after 
-     * modification.
-     * @param d
-     * @param c
-     * @return
-     */
-    private int getNewCaretOffset(IDocument d, DocumentCommand c) {
-        return c.offset + ld.length() + tools.getIndentation(d,c).length();
-    }
-    
-}
+ }

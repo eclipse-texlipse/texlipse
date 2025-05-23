20b937d71c5b0b63055ddb3508e736bc905f486e
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2025-05-23 16:22:01 +0300

    [#76] Update license to EPL 2.0
    * updated license information to EPL 2.0

--- org.eclipse.texlipse/cppsource/ddeclient.cpp
+++ org.eclipse.texlipse/cppsource/ddeclient.cpp
@@ -1,10 +1,15 @@
-/**
- * Copyright (c) 2004-2017 the TeXlapse team.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
+/*******************************************************************************
+ * Copyright (c) 2004, 2025 TeXlipse and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     The TeXlipse team - initial API and implementation
+ *******************************************************************************/
 
 /**
  * @author Tor Arne Vestbø

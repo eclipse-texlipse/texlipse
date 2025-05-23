18294311c9547fd059bf1943c96dcf1c0252b2d4
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2025-05-23 16:22:01 +0300

    [#76] Update license to EPL 2.0
    * updated license information to EPL 2.0

--- NOTICE.md
+++ NOTICE.md
@@ -17,10 +17,10 @@ source code repository logs.
 ## Declared Project Licenses
 
 This program and the accompanying materials are made available under the terms
-of the Eclipse Public License v1.0 which is available at
-https://www.eclipse.org/legal/epl-v10.html.
+of the Eclipse Public License v. 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
 
-SPDX-License-Identifier: EPL-1.0
+SPDX-License-Identifier: EPL-2.0
 
 ## Source Code
 
--- org.eclipse.texlipse/source/org/eclipse/texlipse/editor/TexMathCompletionProcessor.java
+++ org.eclipse.texlipse/source/org/eclipse/texlipse/editor/TexMathCompletionProcessor.java
@@ -1,13 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2017 the TeXlipse team and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2017, 2025 TeXlipse and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     The TeXlipse team - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.texlipse.editor;
 
 import java.util.ArrayList;

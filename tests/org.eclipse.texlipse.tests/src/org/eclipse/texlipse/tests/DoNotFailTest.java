e948fee657ef2beb9454c28a65a21e86f8cc01a5
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2025-05-29 22:06:29 +0300

    [#131] Add test bundle
    * add junit to target
    * add test bundle

---
+++ tests/org.eclipse.texlipse.tests/src/org/eclipse/texlipse/tests/DoNotFailTest.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2025 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation, further support
+ *******************************************************************************/
+
+package org.eclipse.texlipse.tests;
+
+import org.junit.Test;
+
+public final class DoNotFailTest {
+	
+	@Test
+	public void everythingShouldBeGood() {
+		
+	}
+
+}
